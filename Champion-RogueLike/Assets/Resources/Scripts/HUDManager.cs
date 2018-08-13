5376137f5168d87dac51100b3a54ce5a8c19b687
Jared Howell <jhowell713@gmail.com>
2018-08-12 23:45:15 -0400

    Added Health bar and nonfunc mana bar

---
+++ Champion-RogueLike/Assets/Resources/Scripts/HUDManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using RoboRyanTron.Unite2017.Variables;
+
+public class HUDManager : MonoBehaviour
+{
+
+	public FloatVariable hp;
+	public FloatVariable maxHp;
+
+	public Slider healthMeter;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		healthMeter.value = hp.Value / maxHp.Value;
+	}
+}
