2b52a18bc87b009eb418a808160263e0cce95e95
Rilogator <jhowell713@gmail.com>
2018-08-13 21:44:59 -0400

    Added trigger control for attacks
    Maybe work on combo's next?

--- Champion-RogueLike/Assets/Resources/Scripts/AttackScript.cs
+++ Champion-RogueLike/Assets/Resources/Scripts/AttackScript.cs
@@ -4,17 +4,26 @@ using RoboRyanTron.Unite2017.Variables;
 public class AttackScript : MonoBehaviour
 {
 	private Animator anim;
+	private Rigidbody2D myBody;
 
 	// Use this for initialization
 	void Start ()
 	{
 		anim = gameObject.GetComponent<Animator>();
+		//myBody = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		if (Input.GetButtonDown("Fire1"))
+		{
 			anim.SetTrigger("Attack");
+		}
 	}
+
+	//public void StopMove()
+	//{
+	//	myBody.velocity = Vector2.zero;
+	//}
 }
