98f99739bd1f7c6071de74545e4c6a8baa3210f2
Rilogator <jhowell713@gmail.com>
2018-08-16 01:48:13 -0400

    Rolling is Done.
    Need animations, also I should add a vector to the Roll animation once they become directional

---
+++ Champion-RogueLike/Assets/Resources/Scripts/DodgeScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using RoboRyanTron.Unite2017.Variables;
+
+public class DodgeScript : MonoBehaviour
+{
+	public FloatReference dashCD;
+	public FloatReference dashTime;
+	public FloatReference dashPower;
+
+	//public float playerSpeed;
+
+	private Animator anim;
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = gameObject.GetComponent<Animator>();
+		body = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown("Jump"))		
+			anim.SetTrigger("Dodge");		
+	}
+
+	//We might need a container variable to hold the player's pre-dodge speed.
+	//Further testing needed.
+
+		//Don't do this.
+	public void DodgeSpeed()
+	{
+		body.velocity *= 1.20f;
+	}
+
+	public void ReturnNormalSpeed()
+	{
+		//Subtract dashPower from PlayerSpeed.
+	}
+}
