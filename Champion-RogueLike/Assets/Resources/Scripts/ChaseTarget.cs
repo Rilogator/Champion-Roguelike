5e3d0381095f2cda3a794ec665d24e6e7fcb56ea
Rilogator <jhowell713@gmail.com>
2018-08-23 03:44:43 -0400

    hurr

---
+++ Champion-RogueLike/Assets/Resources/Scripts/ChaseTarget.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ChaseTarget : MonoBehaviour
+{
+	public Transform target;
+
+	public int moveSpeed;
+	public int maxDist;
+	public int minDist;
+		   	 
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if (Vector2.Distance(transform.position, target.position) >= minDist)
+		{
+
+			transform.position += (target.position - transform.position).normalized
+				* moveSpeed * Time.deltaTime;
+		}
+	}
+}
