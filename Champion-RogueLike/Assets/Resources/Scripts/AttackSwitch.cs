81e54605a8650020ba725a81722efabd48089bc7
Rilogator <jhowell713@gmail.com>
2018-08-30 09:52:13 -0400

    Added logic for switches

---
+++ Champion-RogueLike/Assets/Resources/Scripts/AttackSwitch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using RoboRyanTron.Unite2017.Variables;
+
+public class AttackSwitch : MonoBehaviour
+{
+	public bool isOn = false;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = gameObject.GetComponent<Animator>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		DamageDealer damage = other.gameObject.GetComponent<DamageDealer>();
+		if (damage != null)
+		{
+			isOn = !isOn;
+			anim.SetTrigger("Toggle");
+		}
+	}
+}
